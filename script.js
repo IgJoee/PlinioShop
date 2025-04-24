384b1604505d9710adede5c8ce59a5b5af4cad2e
IgJoee <00001132548627sp@al.educacao.sp.gov.br>
2025-04-24 12:18:45 -0300

    javascript tema

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function gerarValor(){
     let numero = Math.floor(Math.random()*100)+1;
     document.getElementById("resultado").innerText= "Número Sorteado: " + numero;
 }
+
+let botaotema = document.getElementsByClassName("botao");
+let fundo = document.getElementsByTagName("body");
+
+botaotema.addEventListener("click", function(){
+    fundo.style.backgroundColor = "white";
+})
+
